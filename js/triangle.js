64745aaa8ec61693d3b3519186d8c20cf105564c
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2017-12-30 23:24:45 +0100

    First commit

---
+++ js/triangle.js
@@ -0,0 +1,11 @@
+
+var a = 3;
+var h = 5;
+var triangleArea = a*h/2;
+
+var elArea = document.getElementById('area');
+elArea.textContent = triangleArea;
+
+
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
